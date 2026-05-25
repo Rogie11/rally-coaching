97e55abebd685360cdb7c1732bc93448b5c05bf0
Rogie11 <64162711+Rogie11@users.noreply.github.com>
2026-05-25 20:07:02 +0000

    first try

---
+++ public/firebase-config.js
@@ -0,0 +1,9 @@
+const FIREBASE_CONFIG = {
+  apiKey:            "AIzaSyCOGvXpsDXrLsDT-IqG_Wg_MRwa_26sANE",
+  authDomain:        "reservationrally.firebaseapp.com",
+  projectId:         "reservationrally",
+  storageBucket:     "reservationrally.firebasestorage.app",
+  messagingSenderId: "1090565141679",
+  appId:             "1:1090565141679:web:41abdc9cd94d361e56d05a",
+  measurementId:     "G-0NSY1MPJPW"
+};
